01e4f8d6b8d12803684d93d13212dffcc0803bed
VinothiniS23 <23cs122@drngpit.ac.in>
2026-04-14 12:16:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -3,31 +3,24 @@ class Solution {
         long f= Long.MIN_VALUE;
         long s= Long.MIN_VALUE;
         long t= Long.MIN_VALUE;
-        for (int num: nums) {
-            if (num > f) {
-                f = num;
+        for (int num:nums) {
+            if (num>f) {
+                f= num;
             }
         }
-
-        // Find second max (distinct)
-        for (int num : nums) {
-            if (num < f && num > s) {
-                s = num;
+        for (int num:nums) {
+   if (num<f && num>s) {
+                s= num;
             }
         }
-
-        // Find third max (distinct)
-        for (int num : nums) {
-            if (num < s && num > t) {
-                t = num;
-            }
+        for (int num:nums) {
+              if (num<s && num>t) {
+                = num;
+               }
         }
-
-        // If third doesn't exist
-        if (t == Long.MIN_VALUE) {
-            return (int) f;
+        if (t==Long.MIN_VALUE) {
+              return (int)f;
         }
-
-        return (int) t;
+        return (int)t;
     }
 }
