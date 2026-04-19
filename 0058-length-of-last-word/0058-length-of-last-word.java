42940f35969e72773f2a1c7cbe76944da01ea32e
VinothiniS23 <23cs122@drngpit.ac.in>
2026-04-19 21:10:44 +0530

    Time: 0 ms (100%), Space: 43 MB (71.96%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Solution {
+      public static void main(String args[]){
+      Scanner sc = new Scanner(System.in);
+      String str=sc.nextLine();
+      System.out.println(lengthOfLastWord(str));
+    }
+    static int lengthOfLastWord(String s) {
+      String str =s.trim();
+      int count=0;
+      for(int i=str.length()-1;i>=0;i--){
+        if(str.charAt(i)!= ' '){
+            count++;
+        }
+        else{
+            break;
+        }
+      }
+      return count;
+    }
+}
