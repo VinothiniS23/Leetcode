6e07672d3a9a2bf905485119ee6290df6f439ef7
VinothiniS23 <23cs122@drngpit.ac.in>
2026-05-07 22:44:46 +0530

    Time: 3 ms (87.59%), Space: 43.4 MB (40.66%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch=='('||ch=='['||ch=='{'){
+                st.push(ch);
+            }else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                char c=st.pop();
+                if(ch==')'&&c!='('){
+                    return false;
+                }
+                 if(ch==']'&&c!='['){
+                    return false;
+                }
+                 if(ch=='}'&&c!='{'){
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
